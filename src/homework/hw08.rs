6161a258f8f5772af406e21f822c2ef62b4aae05
Mazalh <135105639+Mazalh@users.noreply.github.com>
2025-03-14 17:26:32 +0200

    Update hw08.rs

---
+++ src/homework/hw08.rs
@@ -0,0 +1,22 @@
+fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+
+    for i in 2..=(n as f64).sqrt() as u32 {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn main() {
+    let num = 17;
+    if is_prime(num) {
+        println!("{} є простим числом", num);
+    } else {
+        println!("{} не є простим числом", num);
+    }
+}
